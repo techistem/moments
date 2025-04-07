44bfa7cbf516f2f980c2de3e566aa53838c22c0c
Istem Karakas <istem.karakas@gmail.com>
2025-04-07 15:03:10 +0100

    Implement axios interceptors to refresh expired access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://ci-drf-api-25-46a75cd721fc.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/from-data';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
+
