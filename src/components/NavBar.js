5ceec53d71049db47088706a053e469bc1979277
Istem Karakas <istem.karakas@gmail.com>
2025-04-08 13:09:57 +0100

    Implement user authentication state using Context API

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
-  const currentUser = useCurrentUser();
-
-  const addPostIcon = (
-    <NavLink
-    className={styles.NavLink}
-    activeClassName={styles.Active}
-    to="/posts/create"
-  >
-    <i className="fas fa-plus-square"></i>Add post
-  </NavLink>
-  )
+  const currentUser = useContext(CurrentUserContext);
 
   const loggedInIcons = <>{currentUser?.username}</>;
   const loggedOutIcons = (
@@ -46,7 +36,7 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-        {currentUser && addPostIcon}
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
